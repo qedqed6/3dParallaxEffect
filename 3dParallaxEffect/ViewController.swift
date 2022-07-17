38f685ced9a9f208c30d454d51f8ef383906dc45
qedqed6 <qedqed6@gmail.com>
2022-07-17 21:10:31 +0800

    Add 3d parallax effect

--- 3dParallaxEffect/ViewController.swift
+++ 3dParallaxEffect/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var foregroundImageView: UIImageView!
+    @IBOutlet weak var middleImageView: UIImageView!
+    
+    var manager = CMMotionManager.init()
+    var label01Frame: CGRect = .zero
+    
+    var backgroundCenter = CGPoint.zero
+    var middleCenter = CGPoint.zero
+    var foregroundCenter = CGPoint.zero
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        manager.startDeviceMotionUpdates(to: .main) {motion, error in
+            guard let attitude = motion?.attitude else {
+                return
+            }
+
+            let eventValue = attitude.roll
+            self.backgroundImageView.center.x = self.backgroundCenter.x + CGFloat(eventValue * -60)
+            self.middleImageView.center.x = self.middleCenter.x + CGFloat(eventValue * -30)
+            self.foregroundImageView.center.x = self.foregroundCenter.x + CGFloat(eventValue * 50)
+        }
     }
 
-
+    override func viewDidLayoutSubviews() {
+        backgroundCenter = backgroundImageView.center
+        middleCenter = middleImageView.center
+        foregroundCenter = foregroundImageView.center
+    }
 }
 
